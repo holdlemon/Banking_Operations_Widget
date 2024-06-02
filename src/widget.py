e3c5c4189913bfaf4d99738b148fbfe35c07e923
daniil vazhov <d.vajov2010@yandex.ru>
2024-06-02 21:28:20 +0300

    add widget

---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from masks import mask_account, mask_card
+
+
+def mask_data(data: str) -> str:
+    name = []
+    number = []
+    for i in data:
+        if i.isalpha():
+            name.append(i)
+        elif i.isdigit():
+            number.append(i)
+
+    if len(number) == 16:
+        return f"{"".join(name)} {mask_card(int("".join(number)))}"
+    else:
+        return f"{"".join(name)} {mask_account(int("".join(number)))}"
+
+
+def convert_date(date: str) -> str:
+    cor_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    cor_date_str = datetime.strftime(cor_date, "%d.%m.%Y")
+    return cor_date_str
