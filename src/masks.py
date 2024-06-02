d5996503256984f33f3e63b803df8808e6085e26
daniil vazhov <d.vajov2010@yandex.ru>
2024-06-02 13:17:19 +0300

    add src

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def mask_card(number_card: int) -> str:
+    """Возвращает маску номера карты"""
+
+    return f"{str(number_card)[:4]} {str(number_card)[4:6]}** **** {str(number_card)[12:]} "
+
+
+def mask_account(number_account: int) -> str:
+    """Возвращает маску номера счета"""
+
+    return f"** {str(number_account)[-4:]}"
