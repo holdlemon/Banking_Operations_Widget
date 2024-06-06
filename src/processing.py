0455cb5fdaf4965dec191213ebb156a8e9d7f6ad
daniil vazhov <d.vajov2010@yandex.ru>
2024-06-06 20:17:39 +0300

    fixes processing.py

--- src/processing.py
+++ src/processing.py
@@ -2,6 +2,8 @@ from typing import Any, Dict, List
 
 
 def filter_by_state(list_of_dict: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """ Возвращает список словарей со значением ключа state """
+
     result = []
     for dictionary in list_of_dict:
         if dictionary["state"] == state:
@@ -10,4 +12,6 @@ def filter_by_state(list_of_dict: List[Dict[str, Any]], state: str = "EXECUTED")
 
 
 def sort_by_date(list_of_dict: List[Dict[str, Any]], ascending: bool = True) -> List[Dict[str, Any]]:
+    """ Возвращает отсортированный список словарей по дате """
+
     return sorted(list_of_dict, key=lambda x: x["date"], reverse=ascending)
